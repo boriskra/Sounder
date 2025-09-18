5f1d028d5b188fae073d410826375d512edb8bab
Boris Krasnoiarov <84999370+boriskra@users.noreply.github.com>
2025-09-17 20:28:03 -0700

    Initial commit from Specify template

---
+++ Sounder/SounderApp.swift
@@ -0,0 +1,32 @@
+//
+//  SounderApp.swift
+//  Sounder
+//
+//  Created by Boris Krasnoiarov on 9/17/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SounderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
